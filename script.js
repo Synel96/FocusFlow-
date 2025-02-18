b3b427a2e3b690dd58751e4d6dba8e6978d20492
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-02-18 16:46:26 +0100

    Remove Button
    adding remove button . can handle enter button now.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+"use strict";
+
 const myList = document.getElementById("myList");
 
 function addItem() {
+  // Add List Item
   let newItem = document.getElementById("newItem").value;
+  if (newItem.trim() === "") {
+    alert("Please enter a to-do first!(Or you have nothing to-do?😉)");
+    return;
+  }
   let li = document.createElement("li");
 
   li.textContent = newItem;
   myList.appendChild(li);
 
   document.getElementById("newItem").value = "";
+
+  //Create remove button
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "X";
+  removeButton.classList.add("remove");
+  removeButton.onclick = () => li.remove();
+  li.appendChild(removeButton);
 }
+
+document
+  .getElementById("newItem")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addItem();
+    }
+  });
