4384436244eeb78253fceaa4b9225805873d49d8
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-02-24 10:17:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,21 +14,40 @@ function addItem() {
   li.textContent = newItem;
   myList.appendChild(li);
 
-  document.getElementById("newItem").value = "";
+  //Create span element
+  let text = document.createElement("span");
+  text.classList.add("text");
+  text.textContent = newItem;
+  li.appendChild(text);
+
+  //Create Button Container
+  let buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("buttonCon");
+  li.appendChild(buttonContainer);
 
   //Create remove button
   let removeButton = document.createElement("button");
   removeButton.textContent = "X";
   removeButton.classList.add("remove");
   removeButton.onclick = () => li.remove();
-  li.appendChild(removeButton);
-}
+  buttonContainer.appendChild(removeButton);
 
+  //Create tick button ✅
+  let tickButton = document.createElement("button");
+  tickButton.textContent = "✅";
+  tickButton.classList.add("tick");
+  tickButton.onclick = () => {
+    li.classList.toggle("completed");
+  };
+  buttonContainer.appendChild(tickButton);
+
+  document.getElementById("newItem").value = "";
+}
+// Calling the add function while pushing enter
 document
   .getElementById("newItem")
   .addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      event.preventDefault();
       addItem();
     }
   });
